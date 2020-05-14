ee7dac79690b608f2b2ed733c66cb00f27c4db87
David <davidcantumartinez@gmail.com>
2020-05-13 21:17:09 -0500

    Finished queries

--- Northwind/Queries.sql
+++ Northwind/Queries.sql
@@ -13,14 +13,32 @@ SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas en 1996]
 FROM [Order Details] od, Orders o
 WHERE YEAR(o.OrderDate) = '1996' AND o.OrderID = od.OrderID
 
-
 /* Querie 3*/
 SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas en 1997]
 FROM [Order Details] od, Orders o
 WHERE YEAR(o.OrderDate) = '1997' AND o.OrderID = od.OrderID
 
-
 /* Querie 4*/
 SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas Totales]
 FROM [Order Details] od
+
+/*Querie 5*/
+SELECT  Top(1) e.Region as [Region con mas ventas en 1997], SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Total en ventas]
+FROM  Orders o, [Order Details] od, Employees e
+WHERE YEAR(o.OrderDate) = '1997' AND o.OrderID = od.OrderID AND o.EmployeeID = e.EmployeeID
+GROUP BY e.Region
+ORDER BY [Total en ventas] DESC
+
+/*Querie 6*/
+SELECT  Top(1) e.City as [Ciudad  con mas ventas en 1997], SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Total en ventas]
+FROM  Orders o, [Order Details] od, Employees e
+WHERE YEAR(o.OrderDate) = '1997' AND o.OrderID = od.OrderID AND o.EmployeeID = e.EmployeeID AND e.Region = 'WA'
+GROUP BY e.City
+ORDER BY SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) DESC
+
+/*Querie 7*/
+SELECT  e.Region, e.Country, SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [total de ventas]
+FROM  Orders o, [Order Details] od, Employees e
+WHERE o.OrderID = od.OrderID AND o.EmployeeID = e.EmployeeID
+GROUP BY e.Region, e.Country
 
