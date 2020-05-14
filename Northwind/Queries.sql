de0c698e1d47be0a0bfeb5abdaf00eb878ca7700
David <davidcantumartinez@gmail.com>
2020-05-13 21:15:45 -0500

    Adding queries

--- Northwind/Queries.sql
+++ Northwind/Queries.sql
@@ -7,3 +7,20 @@ Where ord.ProductID = pr.ProductID and ord.OrderId = o.OrderId and
 Year(o.OrderDate) = 1996
 GROUP BY pr.ProductName, ord.ProductID, Year(o.OrderDate)
 ORDER BY sum(ord.quantity) DESC;
+
+/* Querie 2*/
+SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas en 1996]
+FROM [Order Details] od, Orders o
+WHERE YEAR(o.OrderDate) = '1996' AND o.OrderID = od.OrderID
+
+
+/* Querie 3*/
+SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas en 1997]
+FROM [Order Details] od, Orders o
+WHERE YEAR(o.OrderDate) = '1997' AND o.OrderID = od.OrderID
+
+
+/* Querie 4*/
+SELECT SUM(od.UnitPrice * od.Quantity * (1-od.Discount)) as [Ventas Totales]
+FROM [Order Details] od
+
